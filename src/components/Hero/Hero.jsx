cc897009a2841834fa570a6a6ac43d22adc1ab50
quanganh-snake <tbquanganh@gmail.com>
2024-01-04 21:43:20 +0700

    upload first code clothing shop ecommerce

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import hand_icon from './../../assets/hand_icon.png';
+import arrow_icon from './../../assets/arrow.png';
+import hero_image from './../../assets/hero_image.png';
+
+export const Hero = () => {
+  return (
+		<div className="hero py-12 grid grid-cols-12 items-center justify-center" style={{background: 'linear-gradient(180deg, #fde1ff, #e1ffea22 60%)'}}>
+			<div className="hero-left col-span-8 flex flex-col items-center justify-start">
+				<h2 className='uppercase font-bold'>NEW ARRIVALS ONLY</h2>
+				<div className="font-bold text-7xl my-6">
+					<div className="flex items-center">
+						<p>new</p>
+						<img src={hand_icon} alt="" />
+					</div>
+					<p>collections</p>
+					<p>for everyone</p>
+				</div>
+				<div className="hero-latest-btn px-6 py-3 bg-red-400 flex items-center justify-around gap-4 rounded-full hover:opacity-70 cursor-pointer">
+					<p className="text-white font-medium">Latest Collection</p>
+					<img src={arrow_icon} alt="" />
+				</div>
+			</div>
+			<div className="hero-right col-span-4 h-full">
+				<div className="w-full h-full">
+					<img src={hero_image} alt="hero image" />
+				</div>
+			</div>
+		</div>
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+import logo from "./../../assets/logo.png";
+import cart_icon from "./../../assets/cart_icon.png";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
+
+const NAV_MENU = [
+	{
+		id: 1,
+		name: "shop",
+		path: "/",
+	},
+	{
+		id: 2,
+		name: "men",
+		path: "/men",
+	},
+	{
+		id: 3,
+		name: "women",
+		path: "/women",
+	},
+	{
+		id: 4,
+		name: "kids",
+		path: "/kids",
+	},
+];
+
+const Navbar = () => {
+
+    const [currentMenu, setCurrentMenu] = useState(0);
+
+	return (
+		<div className="w-full bg-white shadow-md">
+			<div className="navbar max-w-screen-2xl mx-auto flex items-center justify-around">
+				<div className="nav__logo flex items-center gap-4 cursor-pointer">
+					<img src={logo} alt="" />
+					<p className="flex flex-col">
+						<span className="font-bold">CLOTHING</span>
+						<span className="italic">Shop</span>
+					</p>
+				</div>
+				<ul className="nav__menu flex items-center gap-12">
+					{NAV_MENU.map((iMenu, index) => {
+						const iMenuActive = currentMenu === index;
+						const handleActiveMenu = () => {
+							setCurrentMenu(index);
+						};
+						return (
+							<li onClick={handleActiveMenu} className="cursor-pointer" key={iMenu.id}>
+								<Link to={iMenu.path} className={clsx("capitalize hover:opacity-70", iMenuActive ? "border-b-2 border-red-500" : null)}>
+									{iMenu.name}
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+				<div className="nav__login-cart flex items-center gap-5">
+					<Link to="/login" className="block text-center w-40 py-2 border border-slate-300 rounded-full hover:bg-slate-100">
+						Login
+					</Link>
+					<Link to="/cart" className="block relative cursor-pointer">
+						<img src={cart_icon} alt="" />
+						<span className="absolute w-5 h-5 text-[12px] flex items-center justify-center -top-1 -right-2 bg-red-500 text-white p-2 rounded-full">0</span>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
